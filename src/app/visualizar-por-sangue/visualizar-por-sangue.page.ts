3adafe55bc4852530ca51413f38351fd89ae3bda
Daniel Lima <danieluncisalsi@hotmail.com>
2019-12-02 19:34:36 -0300

    alterando login

--- src/app/visualizar-por-sangue/visualizar-por-sangue.page.ts
+++ src/app/visualizar-por-sangue/visualizar-por-sangue.page.ts
@@ -11,11 +11,11 @@ export class VisualizarPorSanguePage implements OnInit {
 
   doadores: any;
   ibge = 'https://servicodados.ibge.gov.br/api/v2/censos/nomes/daniel%7Ckelvin%7Ccharles%7Calyne%7CRony%7CAlex'; 
-
+  empresa = 'http://www.transparencia.gov.br/api-de-dados/cepim?pagina=1';
   constructor(private http: HttpClient) { }
 
   getDoadores():Observable<any>{
-    return this.http.get<any>(this.ibge);
+    return this.http.get<any>(this.empresa);
   }
 
   ngOnInit() {
